1a279242df87f14794322e4a25ecc899e4dc4a52
sendilien7 <sendilien23@gmail.com>
2026-01-26 16:27:44 +0500

    update version

--- Formula/pandev-cli-plugin.rb
+++ Formula/pandev-cli-plugin.rb
@@ -1,8 +1,8 @@
 =begin
 
 after release just update the versions with the new hashes. To get the new hashes you need to run (don't forget to change the version):
-curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.5/pandev-cli-plugin_1.0.5_macOS_amd64.tar.gz | shasum -a 256
-curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.5/pandev-cli-plugin_1.0.5_macOS_arm64.tar.gz | shasum -a 256
+curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.6/pandev-cli-plugin_1.0.6_macOS_amd64.tar.gz | shasum -a 256
+curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.6/pandev-cli-plugin_1.0.6_macOS_arm64.tar.gz | shasum -a 256
 
 !!! YOU HAVE TO CHANGE THE VERSIONS IN 3 LINES !!!
 =end
@@ -10,14 +10,14 @@ curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download
 class PandevCliPlugin < Formula
   desc "Pandev CLI Plugin"
   homepage "https://github.com/pandev-metriks/homebrew-pandev-cli"
-  version "1.0.5"
+  version "1.0.6"
 
   if Hardware::CPU.intel?
-    url "https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.5/pandev-cli-plugin_1.0.5_macOS_amd64.tar.gz"
-    sha256 "4745ebb9ff79459d4f2314136ee36dc8f6ccfe25a394ead149226eb789d780e4"
+    url "https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.6/pandev-cli-plugin_1.0.6_macOS_amd64.tar.gz"
+    sha256 "a177a15191f4fed903d95e1830896234b1a1cdce45cd438486965072f2cf69fb"
   else
-    url "https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.5/pandev-cli-plugin_1.0.5_macOS_arm64.tar.gz"
-    sha256 "b7dbc95c31f4d6d1855a2cb07bc84df8cf3b0e65bae37edd1fc91d3df71cfcbc"
+    url "https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.0.6/pandev-cli-plugin_1.0.6_macOS_arm64.tar.gz"
+    sha256 "e0a882bb6025c8f283cab4c3bae934df42ef26c6b170a8728c752a709283c678"
   end
 
   def install
