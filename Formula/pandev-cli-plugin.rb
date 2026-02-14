eae53ef5361e233f5037756478b928cffc597de5
sendilien7 <sendilien23@gmail.com>
2026-02-14 17:46:56 +0500

    mcp server script is added

--- Formula/pandev-cli-plugin.rb
+++ Formula/pandev-cli-plugin.rb
@@ -4,16 +4,16 @@ After release, update the version and hashes. To get the new hashes run:
 
 after release just update the versions with the new hashes. To get the new hashes you need to run (don't forget to change
 the version):
-curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.2.6/pandev-cli-plugin_1.2.6_macOS_amd64.tar.gz | shasum -a 256
-curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.2.6/pandev-cli-plugin_1.2.6_macOS_arm64.tar.gz | shasum -a 256
-curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v1.2.6/pandev-cli-plugin_1.2.6_Linux_amd64.tar.gz | shasum -a 256
+curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v2.0.0/pandev-cli-plugin_2.0.0_macOS_amd64.tar.gz | shasum -a 256
+curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v2.0.0/pandev-cli-plugin_2.0.0_macOS_arm64.tar.gz | shasum -a 256
+curl -sL https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v2.0.0/pandev-cli-plugin_2.0.0_Linux_amd64.tar.gz | shasum -a 256
 !!! YOU HAVE TO CHANGE THE VERSIONS IN 4 LINES !!!
 =end
 
 class PandevCliPlugin < Formula
   desc "Pandev CLI Plugin"
   homepage "https://github.com/pandev-metriks/homebrew-pandev-cli"
-  version "1.2.6"
+  version "2.0.0"
 
   depends_on "jq"
   depends_on "git"
@@ -21,16 +21,16 @@ class PandevCliPlugin < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v#{version}/pandev-cli-plugin_#{version}_macOS_amd64.tar.gz"
-      sha256 "5f6a28fd60c0b6b3fea4236dbf844a0e306822f7395c64299feed6a2aaa42d0c"
+      sha256 "dd5bcf4a1efd88c31150f79b46fffef0ed66619278c866ffc96678c7a410b929"
     else
       url "https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v#{version}/pandev-cli-plugin_#{version}_macOS_arm64.tar.gz"
-      sha256 "e4ef6988e36ae874137aeb67f045aafac4b0563c9bb1f43f2a84e53a73f4dca0"
+      sha256 "7c65926440131afd897ab401f6f53637004ceb0c36d77d435dcbf55f08c96c7d"
     end
   end
 
   on_linux do
     url "https://github.com/pandev-metriks/homebrew-pandev-cli/releases/download/v#{version}/pandev-cli-plugin_#{version}_Linux_amd64.tar.gz"
-    sha256 "3b6b5007613dd7a3e96ea6a8eb0e18cee3d6abf7381e384e9ccfed16942c743f"
+    sha256 "5149d92a5f47b604c7315010a03bbe527c339a2b25e301cf0e72dab6223f69c9"
   end
 
   def install
